db3ab816c59cb9216af62de6d753e453a21bdfba
yeb02 <alpha.blum@gmail.com>
2023-07-15 12:37:10 +0200

    modified params

--- 1D_2_2D.py
+++ 1D_2_2D.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+from torch.nn.modules import Module
 
 from numpy.random import randn
 
@@ -10,33 +11,41 @@ import sys
 
 import matplotlib.pyplot as plt
 
-inS = 1
-outS = 2
+inS = 4
+outS = 16
 
-interS = 64
+interS = 32
 
-nL = 5
+nL = 6
 
-bInit = .1
+batchS = 50
 
-batchS = 100
+lr = .01
 
-lr = .02
+nAnchors = 10
+anchorD = .5/math.sqrt(outS)
 
+class sine(Module):
+    def forward(self, z):
+        return torch.sin(z)
+        
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        l = [nn.Linear(inS, interS)] + nL * [nn.Linear(interS, interS)] + [nn.Linear(interS, outS)]
-        a = [nn.Tanh()] * (nL+2)
+        l = [nn.Linear(inS, interS, bias=False)] + nL * [nn.Linear(interS, interS, bias=False)] + [nn.Linear(interS, outS, bias=False)]
+        a = [nn.ReLU()] * (nL+1) + [nn.Tanh()]
+        # a = [nn.ReLU()] * (nL+1) + [sine()]
+        # a = [sine()] * (nL+2)
+        # a = [nn.Tanh()] * (nL+2)
         self.layer = nn.Sequential(
           *[l[i//2] if i%2==0 else a[i//2] for i in range(2*(nL+2))]
         )
 
         for i in range(nL+2):
             torch.nn.init.xavier_normal_(self.layer[2*i].weight)
-            # torch.nn.init.uniform_(self.layer[2*i].bias, -bInit,bInit)
 
-            self.layer[2*i].bias.data.fill_(0.0)
+            # torch.nn.init.uniform_(self.layer[2*i].bias, -bInit,bInit) #I disabled biases entirely
+            # self.layer[2*i].bias.data.fill_(0.0)
 
 
     def forward(self,x):
@@ -61,8 +70,7 @@ def plot(net):
     plt.show()
 
 net = Net()
-# sys.exit(0)
-optimizer = optim.Adadelta(net.parameters(), lr=lr)
+optimizer = optim.Adadelta(net.parameters(), lr=lr,  weight_decay=0) #SGD or Adadelta ?
 
 data = torch.Tensor(randn(batchS, inS))
 target = torch.clamp(torch.Tensor(randn(batchS, outS))*.3, -1, 1)
@@ -94,23 +102,38 @@ def trainStake(net):
     net.train()
     SFs = []
     es = []
-    for i in range(100000):
+    for i in range(50000):
         Y = torch.clamp(torch.Tensor(randn(1, outS))*.3, -1, 1)
         Xs = torch.Tensor(randn(batchS, inS))
         
         with torch.no_grad():
             bestXid = torch.argmin(torch.norm(net(Xs) - Y.repeat(batchS, 1), dim = 1))
+            X = Xs[bestXid][None, :]
+            # anchors = torch.Tensor(randn(nAnchors, inS)) * anchorD + X.repeat(nAnchors, 1)
+            # anchorsY = net(anchors)
 
-        y = net(Xs[bestXid][None, :]) #reshape to [1, inS]
+        
         optimizer.zero_grad()
+        y = net(X)
         loss = F.mse_loss(y, Y)
         loss.backward()
+        lrFactor = loss.item() if loss.item() > 1 else loss.item()**2
+        optimizer.lr = lr * lrFactor 
+        optimizer.step()
+        
+        
+        # optimizer.zero_grad()
+        # newAnchorsY = net(anchors)
+        # loss = F.mse_loss(newAnchorsY, anchorsY)
+        # loss.backward()
+        # # optimizer.lr = optimizer.lr / nAnchors # I should normalize the grad.
+        # optimizer.lr = optimizer.lr * .5
+        # optimizer.step()
+
         if i % 1000 == 0:
             es.append(i)
             SFs.append(computeSF(net).item())
             print("{:.6f}".format(SFs[-1]))
-        optimizer.lr = lr * loss.item() #important
-        optimizer.step()
     
     plt.plot(es, SFs, 'r-')
     plt.show()
@@ -132,9 +155,9 @@ print(computeSF(net))
 print(computeSF(net))
 print(computeSF(net))
 print(computeSF(net))
-plot(net)
+# plot(net)
 trainStake(net)
-plot(net)
+# plot(net)
 print(computeSF(net))
 print(computeSF(net))
 print(computeSF(net))
